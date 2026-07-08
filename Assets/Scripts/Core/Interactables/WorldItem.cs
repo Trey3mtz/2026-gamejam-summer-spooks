e04501d5a446cff7e333983d1004edb865f019af
Enrique Martinez <112442527+Trey3mtz@users.noreply.github.com>
2026-07-08 14:11:55 -0500

    Create WorldItem.cs

---
+++ Assets/Scripts/Core/Interactables/WorldItem.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using SpookyGame.Core.Item_System;
+using SpookyGame.Player;
+
+namespace SpookyGame.Core.Interactables
+{
+    public class WorldItem : Interactable
+    {
+        [SerializeField] private ItemDefinition _itemDef;
+        
+        public override bool CanInteract(GameObject interactor)
+        {
+            return true; // TryAddItem() already knows if something can't be added
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        public override void Interact(GameObject interactor)
+        {
+            var player = interactor.TryGetComponent<Player>();
+            if(!player) return;
+            Debug.Log("Interacting with " + gameObject.name);
+          
+            player.Inventory.TryAddItem(_itemDef);
+        }
+    }
+}
