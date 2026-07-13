da67b796237e0d7a2fd97fda0d8d83162665d00c
Enrique Martinez <112442527+Trey3mtz@users.noreply.github.com>
2026-07-13 16:00:08 -0500

    Update Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -30,92 +30,19 @@ namespace SpookyGame.Player
         //  Item States
         // ================================================================
 
-        // Out of 100. 0 means no battery.
-        private int _flashlightBatteryLife = 100;
-        private int _blacklightBatteryLife = 100;
-
-        // How long its been on.
-        private float _flashlightOnDuration = 0f;
-        private float _blacklightOnDuration = 0f;
-
-        // The toggle state
-        private bool _flashlightOn = false;
-        private bool _blacklightOn = false;
+        [SerializeField] private BatteryLight _flashlight = new BatteryLight();
+        [SerializeField] private BatteryLight _blacklight = new BatteryLight();
 
 
         // ================================================================
         //  Item API & helper methods
         // ================================================================
 
-        public bool ToogleFlashlight()
-        {
-            if(_flashlightOn)
-                _flashlightOn = false;
-            else if(_flashlightBatteryLife > 0)
-                _flashlightOn = true;
-
-            return _flashlightOn;
-        }
-
-        public bool ToogleBlacklight()
-        {
-            if(_blacklightOn)
-                _blacklightOn = false;
-            else if(_blacklightBatteryLife > 0)
-                _blacklightOn = true;
-
-            return _blacklightOn;
-        }
-
+        public BatteryLight Flashlight => _flashlight;
+        public BatteryLight Blacklight => _blacklight;
         
-
-        private void HandleFlashlightUpdate()
-        {
-            // If its off, return
-            if(!_flashlightOn)
-                return;
-
-            // If its on, but out of battery, force off
-            if(_flashlightBatteryLife <= 0)
-            {
-                ToogleFlashlight();
-                _flashlightBatteryLife = 0;
-                _flashlightOnDuration = 0f;
-                return;
-            }
-                
-            // If its on and has battery life
-            _flashlightOnDuration += Time.deltaTime;
-            if(_flashlightOnDuration >= 2)
-            {
-                _flashlightBatteryLife -= 1;
-                _flashlightOnDuration = 0f;
-            }
-        }
-
-        private void HandleBlacklightUpdate()
-        {
-            // If its off, return
-            if(!_blacklightOn)
-                return;
-
-            // If its on, but out of battery, force off
-            if(_blacklightBatteryLife <= 0)
-            {
-                ToggleBlacklight();
-                _blacklightBatteryLife = 0;
-                _blacklightOnDuration = 0f;
-                return;
-            }
-                
-            // If its on and has battery life
-            _blacklightOnDuration += Time.deltaTime;
-            if(_blacklightOnDuration >= 2)
-            {
-                _blacklightBatteryLife -= 1;
-                _blacklightOnDuration = 0f;
-            }
-        }
+        public bool ToggleFlashlight() => _flashlight.Toggle();
+        public bool ToggleBlacklight() => _blacklight.Toggle();
 
 
         // ================================================================
@@ -124,8 +51,8 @@ namespace SpookyGame.Player
 
         private void Update()
         {
-            HandleFlashlightUpdate();
-            HandleBlacklightUpdate();
+            _flashlight.Tick(Time.deltaTime);
+            _blacklight.Tick(Time.deltaTime);
         }
         
         // ================================================================
